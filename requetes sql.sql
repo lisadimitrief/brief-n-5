a9d06f68e0209f8693c2bada855d8a313cdfabed
lisadimitrief <84084978+lisadimitrief@users.noreply.github.com>
2023-04-19 11:36:33 +0200

    lorem

--- requetes sql.sql
+++ requetes sql.sql
@@ -2,11 +2,9 @@
 -- select * from abonnements;
 -- select * from membre;
 
-
 -- Utiliser une view
 -- select * from mbr;
 -- select * from pigeon;
-
 
 -- faire un join
 -- select * from abonnements join acces_salles using(abonnements_id) join avantage using(avantage_id);
@@ -16,4 +14,10 @@
 acces_salles.avantage_id as "salle basique pour les feignants",
  acces_salles_velo.avantage_id as "salle cycliste arrogant",
  acces_salles_rameur.avantage_id as "salle pour les rameur"
-from membre join abonnements using(abonnements_id) join acces_salles using(abonnements_id) join avantage using(avantage_id) join acces_salles_rameur using(abonnements_id) join acces_salles_velo using(abonnements_id) order by "Prénom Nom";
+from membre join abonnements using(abonnements_id) join acces_salles using(abonnements_id) join avantage using(avantage_id) join acces_salles_rameur using(abonnements_id) join acces_salles_velo using(abonnements_id) order by "Prénom Nom";*/
+
+-- insert into abonnements (nom, tarif, hygiene, coach) values ("bruh", 400, 1, 1);
+-- select * from abonnements;
+
+-- compte toute les colonne d'une table
+SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'membre';
