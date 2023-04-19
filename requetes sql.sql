608fb81f9ad04792c207355410d23fc21e6d7952
lisadimitrief <84084978+lisadimitrief@users.noreply.github.com>
2023-04-19 16:47:29 +0200

    requete

--- requetes sql.sql
+++ requetes sql.sql
@@ -11,13 +11,22 @@
 
 -- rendre joli un join avec des as et un ordre
 /*select membre.nom as "Prénom Nom", login, mot_de_passe as mdp, tarif, 
-acces_salles.avantage_id as "salle basique pour les feignants",
- acces_salles_velo.avantage_id as "salle cycliste arrogant",
- acces_salles_rameur.avantage_id as "salle pour les rameur"
+avantage_id.acces_salles as "salle basique pour les feignants",
+ acces_salles_velo as "salle cycliste arrogant",
+ acces_salles_rameur as "salle pour les rameur"
 from membre join abonnements using(abonnements_id) join acces_salles using(abonnements_id) join avantage using(avantage_id) join acces_salles_rameur using(abonnements_id) join acces_salles_velo using(abonnements_id) order by "Prénom Nom";*/
+
+select membre.nom as "Prénom Nom", avantage, avantage 
+from membre join abonnements using(abonnements_id) 
+join acces_salles using(abonnements_id) join avantage on acces_salles.avantage_id = avantage.avantage_id
+join acces_salles_rameur using(abonnements_id) join avantage on acces_salles_rameur.avantage_id = avantage.avantage_id
+join acces_salles_velo using(abonnements_id) join avantage on acces_salles_velo.avantage_id = avantage.avantage_id;
+
 
 -- insert into abonnements (nom, tarif, hygiene, coach) values ("bruh", 400, 1, 1);
 -- select * from abonnements;
 
 -- compte toute les colonne d'une table
-SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'membre';
+-- SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'membre';
+
+-- select membre.nom, club.nom from membre join club using(club_id)
